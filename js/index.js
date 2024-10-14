7ebeed23b645548230d5a90dd551447c94108886
xiker <xikerromero@gmail.com>
2024-10-14 14:18:46 +0200

    añadidos escuchadores de eventos

--- js/index.js
+++ js/index.js
@@ -9,17 +9,37 @@ document.addEventListener('DOMContentLoaded', function() {
         // Hide loading overlay
 
         console.log("XXXXXXXXXXXXXXX.................arjs-nft-loaded");
+    });
+
+      document.querySelector('a-scene').addEventListener('loaded', () => {
+        console.log('XXXXXXXXX...............Escena completamente cargada');
       });
 
 
+      document.querySelector('[gltf-model]').addEventListener('loaded', () => {
+        console.log('XXXXXXXXXX............Modelo GLTF cargado');
+      })
 
+      document.body.addEventListener('arjs-initialized', (event) => {
+        console.log('XXXXXXXXXXX.............AR.js está inicializado');
+      });
 
+      // Detectar cuándo se ha encontrado el marcador
+        document.querySelector('a-nft').addEventListener('markerFound', () => {
+            console.log('XXXXXXXXXXXX..............Marcador encontrado');
+        });
+  
+        // Detectar cuándo se pierde el marcador
+        document.querySelector('a-nft').addEventListener('markerLost', () => {
+            console.log('XXXXXXXXXXXXXX.............Marcador perdido');
+        });
 
+        document.body.addEventListener('xrSessionStarted', (event) => {
+            console.log('XXXXXXXXXXXXX...........La sesión WebXR ha comenzado');
+          });
 
-
-
-
-
-
+        document.body.addEventListener('xrSessionEnded', (event) => {
+            console.log('XXXXXXXXXX.............La sesión WebXR ha terminado');
+        });
 
 });//fin dom loaded
