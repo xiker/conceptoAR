f6e771906410897454c62fc6e3730b16f17852f7
xiker <xikerromero@gmail.com>
2024-10-14 14:09:18 +0200

    leyendo eventos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("Contenido cargado.....");
-});
+    console.log("XXXXXXXXXXXXXX...........Contenido cargado.....");
+
+
+
+
+
+    window.addEventListener("arjs-nft-loaded", (event) => {
+        // Hide loading overlay
+
+        console.log("XXXXXXXXXXXXXXX.................arjs-nft-loaded");
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+});//fin dom loaded
